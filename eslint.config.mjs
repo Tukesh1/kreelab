71ef7f558716022893fc23e6a720671916fcc865
tukesh1 <tukeshkrraju1011@gmail.com>
2025-01-27 10:06:51 +0530

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Disables the unused variables rule
+      "@typescript-eslint/no-explicit-any": "off", // Allows the use of 'any' type
+      "@typescript-eslint/no-unused-expressions": "off", // Disables the unused expressions rule
+      "react-hooks/exhaustive-deps": "off", // Disables exhaustive dependencies for React hooks
+    },
+  },
 ];
 
 export default eslintConfig;
